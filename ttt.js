baba38137cc132f45ea8e16149a0bce805730294
Aaron Tagliaboschi <aaron.tagliaboschi@gmail.com>
2020-07-25 17:03:02 -0400

    Added a couple more comments

--- ttt.js
+++ ttt.js
@@ -16,6 +16,9 @@
  * c = coordinate object
  */
 
+/* Simple object to pass position. 
+ * It was either this, 0-8, or arrays, and this seemed the most readable
+ */
 function Pos(x,y) {
   this.x = x;
   this.y = y;
@@ -23,6 +26,11 @@ function Pos(x,y) {
 
 equal_p = (list, p) => list.every(a => a == p)
 
+/* (in a Jacques Cousteau voice) zee main function. 
+ *
+ * Basically everything you'd want to do to or with the board state. 
+ * Descriptions inline
+ */
 function TTT() {
   this.board = [
     [0,0,0],
@@ -39,14 +47,14 @@ function TTT() {
     this.board[2].slice(),
   ];
 
-/* We'll check if inserting at a particular cell means a win, because it means
- * less typing for me
- *
- * note: Let's get this out of the way now: I know that one letter variable 
- * names are a sin against Knuth, but there's a lot of repetitive boiler plate
- * to describe very simple things and it will drive me crazy. I'll write 
- * comments, I promise.
- */
+  /* We'll check if inserting at a particular cell means a win, because it means
+   * less typing for me
+   *
+   * note: Let's get this out of the way now: I know that one letter variable 
+   * names are a sin against Knuth, but there's a lot of repetitive boiler plate
+   * to describe very simple things and it will drive me crazy. I'll write 
+   * comments, I promise.
+   */
   this.winAt = (c, p) => {
     b = this.bCopy();
     x = c.x;
@@ -61,6 +69,9 @@ function TTT() {
 	    equal_p([b[0][2], b[1][1], b[0][2]], p)    // diagonal 2 (x+y==2)
   };
 
+  /* This is "immutable" part. Instead of editing the object, a copy of the 
+   * object is made with the edit applied
+   */
   this.insert = (c, p) => {
     b = this.bCopy();
     b[c.x][c.y] = p;
@@ -70,15 +81,15 @@ function TTT() {
     return new_board;
   };
 
+  // Really simple, just ennumerate every position with a zero.
   this.empty = () => {
     empties = [];
 
     for(x of [0,1,2])
       for(y of [0,1,2])
-	if(this.board[x][y]==0)
-	  empties.push(new Pos(x,y));
+        if(this.board[x][y]==0)
+      	  empties.push(new Pos(x,y));
 
     return empties;
   }
-
 }
